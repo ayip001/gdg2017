66dcb3a6e4988fbe4de7640ea90134207e71b038
Angus Yip <ayip002@gmail.com>
2017-11-05 07:46:14 -0800

    test if threading works

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # [START app]
 import logging
 
-from flask import Flask, request
+from flask import Flask, request, url_for
+from datetime import datetime, timedelta
 import requests
+import json
+from flask_jsonpify import jsonify
 """import pyrebase"""
 
 """
@@ -31,34 +20,114 @@ firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 """
 
+# init
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST"])
 def index():
-    """data = db.child("data").get()"""
-    return requests.get('https://qr-cart.firebaseio.com/data.json').content
+	return ("index ok")
 
+# shared methods
+@app.route("/login")
+def login():
+	# param: phoneno
+	# if on system
+		# if agent
+			# return phoneno=phoneno, authenticated=2
+		# else is cx
+			# return phoneno=phoneno, authenticated=1
+	# else return phone=phone, authenticated=0
+	phone = str(request.args.get('phone'))
+	cxs = requests.get('https://qr-cart.firebaseio.com/data/customers.json').json()
+	for cx in cxs:
+		if cx is not None and cx['phone'] == phone:
+			return (jsonify({'phone' : cx['phone'], 'authenticated' : 1}))
+	ags = requests.get('https://qr-cart.firebaseio.com/data/agents.json').json()
+	for ag in ags:
+		if ag is not None and ag['phone'] == phone:
+			return (jsonify({'phone' : ag['phone'], 'authenticated' : 2}))
+	return (jsonify({'phone' : phone, 'authenticated' : 0}))
 
-@app.route("/customers")
-def customers():
+@app.route("/getCart")
+def getCart():
+	# param: phoneno
+	# init empty cart json
+	# get database json search by cx phoneno
+	# if no cart
+		# return empty cart
+	# else
+		# return cart
+	phone = str(request.args.get('phone'))
+	cx = requests.get(url_for('getCust', phone=phone, _external=True)).json()
+	return (jsonify(cx))
+
+@app.route("/getTotal")
+def getTotal():
+	# param: phoneno
+	# init total = 0
+	# if getCart() is empty
+		# return 0
+	# else
+		# for item in cart
+			# total += getValue(itemNo)
+		# return total
+	return ("temp getTotal")
+
+@app.route("/productImg/")
+def getProductImg():
+	# param: itemno
+	# get prodimg json search with itemno
+	# return img
+	return ("tmp prod img")
+
+# agent methods
+@app.route("/allCustomers")
+def allCustomers():
+	# no param
+	# get json requests.get('https://qr-cart.firebaseio.com/data/customers.json').content
+	# clean up
+	# return
     return requests.get('https://qr-cart.firebaseio.com/data/customers.json').content
 
-@app.route("/customer/", methods=['GET'])
-def getCustomer():
-    phone = request.args['phone']
-    return requests.get('https://qr-cart.firebaseio.com/data/customers.json?orderBy="phone"&equalTo="'+phone+'"').content
+@app.route("/getCust")
+def getCust():
+	phone = str(request.args.get('phone'))
+	cxs = requests.get('https://qr-cart.firebaseio.com/data/customers.json').json()
+	for cx in cxs:
+		if cx is not None and cx['phone'] == phone:
+			return (jsonify(cx))
+	return ("not found")
 
-@app.route("/products")
-def products():
-    return "Members"
+@app.route("/confirm")
+def confirm():
+    return ("confirm payment")
 
+@app.route("/endDay")
+def endDay():
+	return ("day concluded")
 
-@app.route("/productImg/", methods=['GET'])
-def getProductImg():
-    product_id = request.args['id']
+# customer methods
+@app.route("/signup")
+def signup():
+	# routed from login
+	return ("tmp signup")
 
-    return requests.get('https://qr-cart.firebaseio.com/data/products_img.json?orderBy="id"&equalTo="'+product_id+'"').content
+@app.route("/enterStore")
+def enterStore():
+	return ("tmp enterStore")
+
+@app.route("/sendAlert")
+def sendAlert():
+	return ("cx nick has entered your store")
+
+@app.route("/addItem")
+def addItem():
+	return ("tmp item added")
+
+@app.route("/updateQty") # to 0 to remove item
+def updateQty():
+	return ("tmp item qty updated")
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True, ssl_context='adhoc')
+    app.run(host='127.0.0.1', port=5000, debug=True, threaded=True)
 # [END app]
